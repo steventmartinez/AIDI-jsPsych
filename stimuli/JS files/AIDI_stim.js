438fff3869cee2130451a786e5d991dc62d9df85
steventmartinez <stevent.martinez@temple.edu>
2024-12-16 14:53:37 -0500

    Initial push

---
+++ stimuli/JS files/AIDI_stim.js
@@ -0,0 +1,35 @@
+var stim = [
+  {
+    "rental_link": "https://www.airbnb.com/rooms/36211766?search_mode=regular_search&check_in=2025-01-10&check_out=2025-01-17&source_impression_id=p3_1734376069_P3Bt-7c-AT6vMyfY&previous_page_section_name=1000&federated_search_id=70bd0486-e2d5-4547-b349-7c9d522da3ad",
+    "rental_company": "AirBnb",
+    "city": "San Diego",
+    "text_stimuli": "Bright, spacious suite in historic home, blocks from downtown and across the street from urban-hip Golden Hill with it's eclectic eateries and coffee houses. Five minute drive or scooter ride to San Diego's historic neighborhoods of North & South Park, Balboa Park, Coronado Beach and the Zoo. Large studio includes Queen bed, Queen sofa sleeper and large attached outdoor patio, private entrance and ample street parking. Great for couples, friends and families of 2-4 people and for longer stays.",
+    "text_type": "Human",
+    "place_stimuli": "stimuli/places/Human - Places/AirBnb_place_sandiego_1_human.png",
+    "place_type": "Human",
+    "face_stimuli": "stimuli/faces/Human - Faces/1.R.EA.M.png",
+    "face_type": "Human"
+  },
+  {
+    "rental_link": "NA",
+    "rental_company": "NA",
+    "city": "San Diego",
+    "text_stimuli": "Step into history with this charming San Diego vacation rental, offering timeless elegance and modern comforts. Perfect for multi-generational travelers, its spacious layout ensures room for everyone. Located just six blocks from downtown, it provides easy access to dining, shopping, and entertainment. Vintage furnishings and original architectural details create a unique, nostalgic ambiance. Whether gathering in the cozy living area or exploring nearby attractions, this suite promises a memorable stay for all ages.",
+    "text_type": "AI",
+    "place_stimuli": "stimuli/places/AI - Places/AirBnb_place_sandiego_1_AI.png",
+    "place_type": "AI",
+    "face_stimuli": "stimuli/faces/AI - Faces/4.S.EA.F.png",
+    "face_type": "AI"
+  },
+  {
+    "rental_link": "https://www.airbnb.com/rooms/1094865994269016556?search_mode=regular_search&check_in=2025-02-19&check_out=2025-02-26&source_impression_id=p3_1734376690_P30Xv5PUaFElgaOA&previous_page_section_name=1000&federated_search_id=a3f0c435-6648-4b5d-9e45-a0ae52b94fe1",
+    "rental_company": "AirBnb",
+    "city": "Tampa",
+    "text_stimuli": "Welcome to your slice of paradise in this stunning Key West style home situated in Tampa's coveted neighborhood of Bayshore Beautiful. This million-dollar oasis is a perfect blend of luxury, comfort, and island charm. Three bed-- 2 1/2 bathrooms will accommodate both the vacationing family or a business traveler. Located moments from Hyde Park Village and Bayshore Blvd. The only downside here is choosing between a culinary adventure or a day exploring one of our world famous beaches!",
+    "text_type": "Human",
+    "place_stimuli": "stimuli/places/Human - Places/AirBnb_place_tampa_2_human",
+    "place_type": "Human",
+    "face_stimuli": "stimuli/faces/Human - Faces/1.R.W.F.png",
+    "face_type": "Human"
+  }
+]
